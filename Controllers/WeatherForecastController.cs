2481858b69f18d727f8c3d5380c626c873f42a24
VarunMehra <varun.mehra942000@gmail.com>
2024-03-15 00:10:49 +0530

    Resolved

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -21,14 +21,14 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-      //somehting to commit
 
-         Enumerable.Range(1, 5).Select(index => new WeatherForecast
+
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
-        .ToArray();
+          .ToArray();
     }
 }
